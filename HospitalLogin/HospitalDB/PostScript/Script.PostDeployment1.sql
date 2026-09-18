a45c82346fa0a7cc01ded242089dde78e3ce1f6b
carai <carai@DESKTOP-CN99MED>
2026-09-18 19:46:19 +0800

    add login database implementation

---
+++ HospitalLogin/HospitalDB/PostScript/Script.PostDeployment1.sql
@@ -0,0 +1,14 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT INTO LoginDB (Username, Password) VALUES ('admin', 'admin123');
+INSERT INTO LoginDB (Username, Password) VALUES ('nurse', 'nurse123');
+INSERT INTO LoginDB (Username, Password) VALUES ('cashier', 'cashier123');
